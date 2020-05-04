0840b2968c7a34ee420e6eb2040dae27dc9de6f7
Justas Sperauskas <justas@justas.eu>
2020-05-04 11:00:19 +0300

    Initial REST test created

---
+++ src/test/java/eu/justas/payments/api/GreetingControllerIntegrationTest.java
@@ -0,0 +1,32 @@
+package eu.justas.payments.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GreetingController.class)
+public class GreetingControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void greeting_endpoint_returns_id_and_name() throws Exception {
+
+        mvc.perform(get("/api/greeting").param("name", "Justas").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.content", is("Hello, Justas!")));
+    }
+
+}
