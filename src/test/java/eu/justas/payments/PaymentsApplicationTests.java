dd69d758974b4fdc059245e23b174da0b2558c64
Justas Sperauskas <justas@justas.eu>
2020-05-04 09:20:34 +0300

    Created app with Spring Initializr

---
+++ src/test/java/eu/justas/payments/PaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.justas.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
